846d910b166a2a9a2523ada1e1eb1bcf16bc5b87
Marcel Schwittlick <marzzzel@gmail.com>
2012-04-24 19:21:11 +0200

    added physics

--- src/run/SpaceMinusTime.java
+++ src/run/SpaceMinusTime.java
@@ -2,30 +2,59 @@ package run;
 
 import peasy.PeasyCam;
 import processing.core.PApplet;
+import toxi.geom.Rect;
+import toxi.geom.Vec2D;
+import toxi.geom.Vec3D;
+import toxi.physics.VerletParticle;
+import toxi.physics.VerletPhysics;
+import toxi.physics.VerletSpring;
+import toxi.physics.behaviors.GravityBehavior;
 
-public class SpaceMinusTime extends PApplet{
-	
+public class SpaceMinusTime extends PApplet {
+
 	PeasyCam cam;
+	VerletPhysics physics;
+	int roadLength = 300;
+	VerletParticle[] particles;
+	VerletSpring[] springs;
 
-	public void setup(){
+	public void setup() {
 		size(500, 300, P3D);
-		cam = new PeasyCam(this, 20);
+		cam = new PeasyCam(this, 200);
+		physics = new VerletPhysics();
+		physics.addBehavior(new GravityBehavior(new Vec3D(0, 0.5f, 0)));
+		initParticles();
 	}
 
-	public void draw(){
+	public void draw() {
 		background(0);
-		noStroke();
-		fill(200);
-		drawPlane();
-	}
-	
-	private void drawPlane(){
-		beginShape(TRIANGLE_STRIP);
-		for(int i=0;i<100; i++){
-			vertex(-10,  i);
-			vertex(10, i);
+		strokeWeight(1);
+		stroke(170);
+		physics.update();
+		for(int i=0; i<springs.length; i++){
+			line(particles[i].x, particles[i].y, particles[i+2].x, particles[i+2].y);
 		}
-		endShape();
+		strokeWeight(3);
+		stroke(255);
+		for(int i=0; i<particles.length; i++){
+			point(particles[i].x, particles[i].y);
+		}
+	}
+
+	private void initParticles() {
+		particles = new VerletParticle[roadLength]; 
+		springs = new VerletSpring[roadLength-2];
+		for(int i=0; i<roadLength; i+=2){
+			particles[i] = new VerletParticle(new Vec3D(-30, 30*i, 0));
+			particles[i+1] = new VerletParticle(new Vec3D(30, 30*i, 0));
+			physics.addParticle(particles[i]);
+			physics.addParticle(particles[i+1]);
+		}
+		for(int i=0;i<springs.length; i++){
+			springs[i] = new VerletSpring(particles[i], particles[i+2], 1, 0.01f);
+			physics.addSpring(springs[i]);
+		}
+		particles[0].lock();
+		particles[1].lock();	
 	}
 }
-
